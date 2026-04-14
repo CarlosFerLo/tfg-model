f1cab86b23f2ab322ab771e667a4d48fadb30ce7
Carlos Fernandez <carlos.ferlo@outlook.com>
2026-04-14 17:55:12 +0200

    first implementation of node embedding and graph to tupple

--- tfg-model/data/graphs_tuple.py
+++ tfg-model/data/graphs_tuple.py
@@ -1,7 +1,9 @@
-from typing import NamedTuple
+from typing import NamedTuple, List
 import jax
 
 class GraphsTuple(NamedTuple):
     nodes: jax.Array
     senders: jax.Array
     receivers: jax.Array
+    
+    uid_mapping: List[str]
--- tfg-model/graph_to_tuple.py
+++ tfg-model/graph_to_tuple.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List
 
 import jax
 import jax.numpy as jnp
@@ -30,35 +30,50 @@ def _embed_type(t: implica.Type, cache: Dict[str, jax.Array], model: Model, rngs
         return out
     else :
         raise RuntimeError("Every type should be either a variable or an arrow, hence this should never happen.")
+     
+def _map_uid_to_int(uids: List[str], uid_mapping: List[str]) -> List[int] :
+    def _uid_to_int(uid: str) -> int :
+        return uid_mapping.index(uid)
         
+    return list(map(_uid_to_int, uids))
 
 def graph_to_tuple(graph: implica.Graph, model: Model, rngs: nnx.Rngs) -> GraphsTuple :
     
     embedding_cache = {}
     nodes = graph.nodes() 
     
+    # Get node uid_mapping and embeddings
     uid_mapping, node_embeddings = zip(*[
         (n.uid(), _embed_type(n.type(), embedding_cache, model, rngs=rngs))
         for n in nodes
     ])
+
     
-    print(uid_mapping)
-    print(node_embeddings)
+    edges = graph.edges()
+    senders_uid, receivers_uid = zip(*[
+        e.uid()
+        for e in edges
+    ])
     
-    return GraphsTuple(jnp.zeros(0), jnp.zeros(0), jnp.zeros(0))
+    senders = _map_uid_to_int(list(senders_uid), list(uid_mapping))
+    receivers = _map_uid_to_int(list(receivers_uid), list(uid_mapping))
+    
+    return GraphsTuple(
+        nodes=jnp.array(node_embeddings),
+        senders=jnp.array(senders),
+        receivers=jnp.array(receivers),
+        uid_mapping=list(uid_mapping)
+    )
 
 if __name__ == "__main__" :
     
     model = Model(10, 10, 10, 10, 10, 2, rngs=nnx.Rngs(1))
     
-    print(model)
-    
     graph = implica.Graph(constants=[implica.Constant("f", "A -> B")])
-    
-    print(graph.nodes())
     
     graph.query().create("(:A)").create("(:B)").create("()-[::@f()]->()").execute()
     
-    graph_to_tuple(graph, model, nnx.Rngs(1))
+    tuple = graph_to_tuple(graph, model, nnx.Rngs(1))
+    print(tuple)
     
     
